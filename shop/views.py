7947d1205c7e7fc0c681ab54084b19e7dacc5437
amirhosein <amirhoseinbu80@gmail.com>
2024-04-23 14:13:51 +0330

    add category to product_detail

--- shop/views.py
+++ shop/views.py
@@ -9,7 +9,7 @@ def product_list(request,category_slug=None):
     products = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category,slug=category_slug)
-        products = Product.objects.filter(category = category)
+        products = Product.objects.filter(category = category,available=True)
     context ={
         'category':category,
         'categories':categories,
@@ -19,7 +19,8 @@ def product_list(request,category_slug=None):
 
 def product_detail(request,product_id,slug):
     product = get_object_or_404(Product,id=product_id,slug=slug,available=True)
+    category = Category.objects.filter(products=product)
     form = CartAddProductForm()
-    return render(request,'shop/products/detail.html',{'product':product,
+    return render(request,'shop/products/detail.html',{'product':product,'category':category,
                                                        'form':form})
 
