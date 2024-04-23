137b841592b3992420e57b2fdc7b632106635b52
amirhosein <amirhoseinbu80@gmail.com>
2024-04-23 14:12:17 +0330

    edit name category slug

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,6 @@ app_name = 'shop'
 
 urlpatterns = [
     path('shop/products/',views.product_list,name='products'),
-    path('<slug:category_slug>/',views.product_list,name='products_list_by_category'),
+    path('<slug:category_slug>/',views.product_list,name='products_by_category'),
     path('<int:product_id>/<slug:slug>/',views.product_detail,name='product_detail'),
 ]
