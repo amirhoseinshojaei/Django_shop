a1a8874b3949a96a8ab93292c3f2ff8e59f28317
amirhosein <amirhoseinbu80@gmail.com>
2024-04-30 01:40:49 +0330

    add paypal to installed apps

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap5',
+    'paypal.standard.ipn',
     # Custom Apps
     "shop.apps.ShopConfig",
     "cart.apps.CartConfig",
