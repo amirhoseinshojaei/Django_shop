02f1e0451373decb59df6fdb24d0c2a05707d388
amirhosein <amirhoseinbu80@gmail.com>
2024-04-30 01:42:11 +0330

    build payment process

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , get_object_or_404
+from decimal import Decimal
+from django.conf import settings
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from paypal.standard.forms import PayPalPaymentsForm
+from order.models import Order
 # Create your views here.
+
+
+def payment_process(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order,id=order_id)
+    host = request.get.host()
+    paypal_dict = {
+        'business': settings.PAYPAL_RECEIVER_EMAIL,
+        'amount': '%.2f' % order.get_total_cost().quantize(
+            Decimal('.01')),
+        'item_name': f'Order {order.id}',
+        'invoice': str(order.id),
+        'currency_code': 'USD',
+        'notify_url': 'http://{}{}'.format(host,
+                                           reverse('paypal-ipn')),
+        'return_url': 'http://{}{}'.format(host,
+                                           reverse('payment:done')),
+        'cancel_return': 'http://{}{}'.format(host,
+                                              reverse('payment:canceled')),
+    }
+    form = PayPalPaymentsForm(initial=paypal_dict)
+    return render(request,
+                  'payment/process.html',
+                  {'order': order, 'form': form})
