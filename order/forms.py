4ec9f3218c053fcee574f024ddd07ca5b4896086
amirhosein <amirhoseinbu80@gmail.com>
2024-04-29 22:21:38 +0330

    build order form

--- order/forms.py
+++ order/forms.py
@@ -1,11 +1,16 @@
 from .models import Order
 from django import forms
 class CreateOrderForm(forms.ModelForm):
-
+    CITY_LIST = [
+        ('a', 'New York'),
+        ('b', 'Los Angles'),
+        ('c', 'Texas'),
+        ('d', 'San Francisco'),
+    ]
     first_name = forms.CharField(max_length=255,label='Enter your name')
     last_name = forms.CharField(max_length=255,label='Enter your last name')
     email = forms.EmailField(label='Enter Your Email')
-    city = forms.CharField(max_length=255,label="Chose your city")
+    city = forms.ChoiceField(choices=CITY_LIST,label='Chose your city',widget=forms.Select)
     address = forms.CharField(max_length=255,label="Enter your address")
     postal_code = forms.CharField(max_length=255,label='Enter Your postal(IMPORTANT)')
     class Meta:
