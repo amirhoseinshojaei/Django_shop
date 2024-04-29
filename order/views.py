9c4102edb2d2f6a304b4297fa74d263bda031240
amirhosein <amirhoseinbu80@gmail.com>
2024-04-29 22:22:21 +0330

    build order_create view

--- order/views.py
+++ order/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from cart.cart import Cart
+from .models import OrderItem
+from .forms import CreateOrderForm
+from .task import order_create
 # Create your views here.
+
+def order_create(request):
+
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = CreateOrderForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order = order,
+                                         product = item['product'],
+                                         price = item['price'],
+                                         quantity = item['quantity'])
+
+            # clear the cart
+            cart.clear()
+            order_create(order.id)
+            request.session['order_id'] = order.id
+            # redirect to payment
+            # todo; build payment process
+
+    else:
+        form = CreateOrderForm()
+
+    return render(request,'orders/order/create.html',{'cart':cart,'form':form})
