f3dc25a949b5c8f3a5386132fbc72d0d3a7c8503
amirhosein <amirhoseinbu80@gmail.com>
2024-05-02 00:05:10 +0330

    install restframework

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'bootstrap5',
     'paypal.standard.ipn',
     # Custom Apps
