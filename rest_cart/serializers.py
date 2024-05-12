2afcb509e81995a599925661b96f546f5bdfd661
amirhosein <amirhoseinbu80@gmail.com>
2024-05-13 00:48:30 +0330

    build CartSerializer

--- rest_cart/serializers.py
+++ rest_cart/serializers.py
@@ -8,3 +8,10 @@ class CartItemSerializer(serializers.Serializer):
     price = serializers.DecimalField(max_digits=10,decimal_places=2)
     total_price = serializers.DecimalField(max_digits=10,decimal_places=2)
 
+
+class CartSerializer(serializers.Serializer):
+
+    items = CartItemSerializer(many=True)
+    total_items = serializers.IntegerField()
+    total_price = serializers.DecimalField(max_digits=10,decimal_places=2)
+
